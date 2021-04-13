a91618f45347ab6dc18d180b6e36be699ba8bc8d
Larry Li <yli478@ucsc.edu>
2021-04-13 18:25:39 +0800

    break

--- cmpm120/src/main.js
+++ cmpm120/src/main.js
@@ -10,3 +10,6 @@ let game = new Phaser.Game(config);
 let borderUISize = game.config.height/15;
 let borderPadding = borderUISize/3;
 let starSpeed=4;
+
+//reserve ketboard bindings
+let keyF, keyR, keyLEFT, keyRIGHT;
--- cmpm120/src/prefabs/Rocket.js
+++ cmpm120/src/prefabs/Rocket.js
@@ -5,5 +5,37 @@ class Rocket extends Phaser.GameObjects.Sprite {
 
         //add object to the existing scene
         scene.add.existing(this);
+        this.isFiring = false; //track rocket firing status
+        this.moveSpeed = 2;    //pixels per frame
+    }
+
+    update(){
+        //left/right movement
+        if(!this.isFiring){
+            if(keyLEFT.isDown && this.x >= borderUISize + this.width){//prevent going through wall
+                this.x -= this.moveSpeed;
+            }else if(keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width){
+                this.x += this.moveSpeed;
+            }
+        }
+        //fire button
+        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+            this.isFiring = true;
+        }
+        // if fired move the rocket up
+        if(this.isFiring && this.y >= borderUISize*3 + borderPadding){
+            this.y -=this.moveSpeed;
+
+        }
+
+        //reset
+        if(this.y <=borderUISize *3 +borderPadding){
+            this.reset();
+        }
+    }
+    //reset rocket to "ground"
+    reset(){
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
 }
