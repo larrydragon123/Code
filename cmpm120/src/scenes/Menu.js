632075ea3d9ed6188054c4e9f64c2ed08dcdbf1c
Larry Li <yli478@ucsc.edu>
2021-04-13 16:55:44 +0800

    rocket implemented

--- cmpm120/src/scenes/Menu.js
+++ cmpm120/src/scenes/Menu.js
@@ -1,9 +1,12 @@
 class Menu extends Phaser.Scene {
-    constructor() {
-      super("menuScene");
-    }
-    
-    create() {
-      this.add.text(20, 20, "Rocket Patrol Menu");
-    }
+  constructor() {
+    super("menuScene");
   }
+
+  create() {
+     this.add.text(20,20,"Rocket Patrol Menu");
+
+     this.scene.start("playScene");
+  }
+}
+// console.log("this");
